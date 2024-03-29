8532c6f0c5aa3e6631baff4ebd4252f5a998a4d7
Lasse <lasse@saarinen.dev>
2024-03-29 09:13:47 +0200

    groups and network

--- devices/desk/desk.nix
+++ devices/desk/desk.nix
@@ -6,12 +6,13 @@ let
 in
 {
   networking.hostName = "desk"; # Define your hostname.
-  networking.interfaces.enp10s0.ipv4.addresses = [ {
-    address = "10.0.0.180";
-    prefixLength = 24;
-  }];
-  networking.defaultGateway = "10.0.0.1";
-  networking.nameservers = [ "10.0.0.3" ];
+  # networking.interfaces.enp10s0.useDHCP = false;
+  # networking.interfaces.enp10s0.ipv4.addresses = [ {
+  #   address = "10.0.0.180";
+  #   prefixLength = 24;
+  # }];
+  # networking.defaultGateway = "10.0.0.1";
+  # networking.nameservers = [ "10.0.0.3" ];
 
   services.ollama.enable = true;
   services.ollama.acceleration = "rocm";
